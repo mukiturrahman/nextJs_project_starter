7d5a28970e2553a17938f9398f6fb12ee31c4f14
mukiturrahman <mukiturrahman1012@gmail.com>
2024-08-04 01:26:46 +0600

    Initi

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Tailwindcss Working Fine</h1>
+            <Button variant="default">ShadCn Working Fine</Button>
+        </div>
+    );
+}
